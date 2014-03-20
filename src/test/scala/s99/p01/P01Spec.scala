6e5e16efd224a67563677489833c7aebc42b0c1d
Radoslaw Wieckowski <radoslaw.wieckowski@gmail.com>
2014-03-20 22:22:31 +0100

    Extended p01 spec

--- src/test/scala/s99/p01/P01Spec.scala
+++ src/test/scala/s99/p01/P01Spec.scala
@@ -3,7 +3,11 @@ package s99.p01
 import s99.UnitSpec
 
 class P01Spec extends UnitSpec {
-  "last" should "find the last element of a list" in {
+  "last" should "find the last element of a list with one element" in {
+    last(List(1)) should equal(1)
+  }
+
+  it should "find the last element of a list" in {
     last(List(1, 1, 2, 3, 5, 8)) should equal(8)
   }
 
